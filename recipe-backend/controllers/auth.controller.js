829c6c5605676b432b86409d39a576e8d6e35f62
Abner Dsouza <abnerdsouza2005@gmail.com>
2025-03-30 03:46:48 +0530

    add route and controller for signup and login

---
+++ recipe-backend/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const signup = async (req, res) => {
+  const { username, email, password, role } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = new User({ username, email, password: hashedPassword, role });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({
+      error: "INTERNAL_SERVER_ERROR",
+      message: err,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.json({
+      token,
+      user: { id: user._id, username: user.username, role: user.role },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
